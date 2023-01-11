2869c2d504a0b07d55874c16db99fd146da59ace
JoaquinSM13 <u201910511@upc.edu.pe>
2023-01-10 21:57:44 -0500

    feat: grep command fixed

--- nmapMultipleIP.sh
+++ nmapMultipleIP.sh
@@ -7,7 +7,7 @@ readarray -t ip_list < ip.txt
 for ip in "${ip_list[@]}"
 do
     nmap -p- -Pn $ip > temp.txt
-    open_ports=$(grep -Eo '[0-9]{1,5}/open' temp.txt | awk -F '/' '{print $1}' | tr '\n' ',')
+    open_ports=$(grep -Eo '[0-9]{1,5}/tcp open' temp.txt | awk -F '/' '{print $1}' | tr '\n' ',')
     nmap -T4 -Pn -A -p $open_ports $ip > $ip.txt
     rm temp.txt
 done
